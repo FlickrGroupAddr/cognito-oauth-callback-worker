f591ba283ce41704c74e34ca81251a32925d99dd
Terry Ott <Terry.Ott@sixbuckssolutions.com>
2021-12-04 00:30:10 +0000

    Now able to access the secrets we need

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,16 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+    // Parse the code parameter out of the URL
+    const { searchParams } = new URL(request.url)
+    const oauth_code:string|null = searchParams.get('code')
+
+
+    // Exchange the oauth code for a token
+
+    // Global variables with secrets we need:
+    //      OAUTH_CLIENT_ID
+    //      OAUTH_CLIENT_SECRET
+     
+
+    //return new Response(`OAuth code: ${oauth_code}, client id: ${OAUTH_CLIENT_ID}`)
+    return new Response(`Code: ${oauth_code}` )
 }
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+export {};
+
+declare global {
+    const OAUTH_CLIENT_ID: STRING
+}
